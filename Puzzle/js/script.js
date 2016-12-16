f59003ec15caf908ca5dfed32698deb0612ec6e4
stormclouddesign <sclouddesign@gmail.com>
2016-12-16 16:53:38 -0500

    init

---
+++ Puzzle/js/script.js
@@ -0,0 +1,15 @@
+
+$( function() {
+	snapfit.defaultMixed=true;
+	snapfit.defaultPolygon= true;
+});
+
+function showImg() {
+	document.getElementById("img1").className = "show";
+
+}
+
+function showImg2() {
+	document.getElementById("img2").className = "show";
+
+}
